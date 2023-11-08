f7c92f12150e86147349a3d8128dc7461cbb1dae
glass91@gmail.com <glass91@gmail.com>
2023-11-08 14:38:18 -0500

    h

--- terraform_ansible_generic_instace_setup_template/instances.tf
+++ terraform_ansible_generic_instace_setup_template/instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "nodejs-app" {
-  ami                    = "ami-053b0d53c279acc90" # Ubuntu server 22.04
-  instance_type          = "t2.micro" # Do not forget to turn off the instance after the test is complete ( around $30 monthly)
+  ami                    = "ami-016485166ec7fa705" # Ubuntu server 22.04
+  instance_type          = "t4g.small" # Do not forget to turn off the instance after the test is complete ( around $30 monthly)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "TestInstance2Last"
   tags = {
